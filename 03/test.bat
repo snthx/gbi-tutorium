00d4024d300e6829a6893953a85989eb37298731
Moritz Laupichler <32931570+molaupi@users.noreply.github.com>
2017-12-19 00:30:49 +0100

    Shit is lit

---
+++ 03/test.bat
@@ -0,0 +1,17 @@
+@echo off
+	set SOURCE=03
+
+	pdflatex .\%SOURCE%.tex
+	pdflatex .\%SOURCE%.tex
+
+	del -f .\*.vrb
+	del -f .\*.aux
+	del -f .\*.log
+	del -f .\*.nav
+	del -f .\*.out
+	del -f .\*.snm
+	del -f .\*.toc
+	del -f .\*.bbl
+	del -f .\*.blg
+	del -f .\*.zip
+	del -f .\*~
